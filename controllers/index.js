af8ee0f8e82d5b40dd37c4c5e7a06185012c8dd5
Sandro Estrada <sandro.ferax@gmail.com>
2022-03-16 09:01:08 -0600

    feat: add stock optimize v1

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const MovementController = require("./movement");
 const DepartmentController = require("./department");
 const ProductController = require("./product");
 const TransferController = require("./transfer");
+const StockController = require("./stock");
 
 module.exports = {
   MovementController,
   DepartmentController,
   ProductController,
   TransferController,
+  StockController,
 };
